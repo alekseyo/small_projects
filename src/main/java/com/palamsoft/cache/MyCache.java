d0e22ee396a6c0acad27d3c672e54ab08776af8b
Aleksey <aleksey232@gmail.com>
2017-02-07 14:27:17 +0300

    Implemented cleanup thread

--- src/main/java/com/palamsoft/cache/MyCache.java
+++ src/main/java/com/palamsoft/cache/MyCache.java
@@ -2,28 +2,29 @@ package com.palamsoft.cache;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.log4j.Logger;
 
+import net.jcip.annotations.GuardedBy;
+
 /**
  * 
- * @author Aleksey
- * 
- * Cache is backed by a list. Nodes are never removed. Each node is assigned an id that is equal to node's index.
+ * Cache is backed by a ArrayList. Nodes are never removed. Each node is assigned an id that is equal to node's index.
  * 
  * The list is guarded by a read-write lock. The only write operation on the list itself is adding a node.
- * Readers can change state of the node (flush to disk, usage counter). So in addition to a read lock 
+ * Readers can change state of the node itself (flush to disk, usage counter). So in addition to a read lock 
  * a reader synchronizes on node's monitor.
  * 
- * Maximum memory size validation is approximate.
+ * Memory usage is controlled by a daemon thread which stores least used entries on disk. Cache signals this
+ * thread that memory limit is reached, the thread frees memory and signals back to the blocked cache methods.
  * 
  */
 public class MyCache {
@@ -31,170 +32,138 @@ public class MyCache {
 	private final Logger logger = Logger.getLogger(this.getClass());
 	
 	private static class Node {
-		private final int id;
-
 		private int usedCount;
 
-		// If null then data is stored on disk
+		// If value == null then data is stored on disk
 		private byte[] value;
+		private final int size;
 		private boolean alreadyOnDisk = false;
 		
-		public Node(int id) {
-			this.id = id;
+		public Node(byte[] value) {
+			this.value = value;
+			this.size = value.length;
 		}
 	}
 
-	// @GuardedBy("this")
+	@GuardedBy("this")
 	private final Map<String, byte[]> disk = new HashMap<String, byte[]>();
 
-	// @GuardedBy("rwl")
+	@GuardedBy("rwl")
 	private final List<Node> cache;
 	
-	private final AtomicLong memorySize = new AtomicLong(); 
+	@GuardedBy("rwl.writeLock")
+	private long memorySize = 0; 
 
 	private final long highMemoryLimit;
 	private final long lowMemoryLimit;
+	private final Thread cleanupThread;
 
 	// fair, so readers and writers get lock "in-order"
 	private final ReadWriteLock rwl = new ReentrantReadWriteLock(true);
-
+	
+	// CONDITION PREDICATE: noMemory (memorySize + loaded_node_size > highMemoryLimit)
+	private final Condition noMemory = rwl.writeLock().newCondition();
+	
+	// CONDITION PREDICATE: memoryAvailable (memory was freed)
+	private final Condition memoryAvailable = rwl.writeLock().newCondition();
+	
 	public MyCache(int initialCapacity, long highLimitBytes, long lowLimitBytes) {
 		this.cache = (initialCapacity == 0) ? new ArrayList<>() 
 				: new ArrayList<Node>(initialCapacity);
 		this.highMemoryLimit = highLimitBytes;
 		this.lowMemoryLimit = lowLimitBytes;
+		cleanupThread = new Thread( new Cleaner() );
+		cleanupThread.setDaemon(true);
 	}
+		
 	public MyCache(long highLimitBytes) {
 		this(0, highLimitBytes, (int) ( highLimitBytes * 0.8 ) );
 	}
 
+
+	// BLOCKS-UNTIL: memoryAvailable
 	public int putToCache(byte[] data) {
-		int newNodeId = 0;
-		
+		int id = 0;
+
 		rwl.writeLock().lock();
+
+		if (cleanupThread.getState() == Thread.State.NEW) {
+			cleanupThread.start();
+		}
+
 		try {
-			newNodeId = cache.size();
-			Node newNode = new Node(newNodeId);
-			newNode.value = Arrays.copyOf(data, data.length);
-			cache.add(newNode);
-			memorySize.addAndGet(newNode.value.length);
+			Node node = new Node(Arrays.copyOf(data, data.length));
+
+			while (node.size + memorySize > highMemoryLimit) {
+				noMemory.signalAll();
+				logger.debug("Waiting: no room for new node");
+				
+				try {
+					if ( !memoryAvailable.await(1000, TimeUnit.MILLISECONDS) ) {
+						continue; // signal again
+					}
+				} catch (InterruptedException e) {
+					throw new RuntimeException("Interrupted");
+				}
+			}
+			id = cache.size();
+			cache.add(node);
+			memorySize += node.size;
+
 		} finally {
 			rwl.writeLock().unlock();
 		}
 
-		ensureMemoryThreshold();
-		
 		if (logger.isInfoEnabled()) {
-			logger.info("Add node: " + newNodeId + ", size (bytes): " + data.length);
+			logger.info("Added node: " + id + ", size (bytes): " + data.length);
 		}
-		
-		return newNodeId;
-	}
 
+		return id;
+	}
+	
+	// BLOCKS-UNTIL: memoryAvailable
 	public byte[] getFromCache(int id) {
 		byte[] result;
 
-		boolean memoryExpanded = false;
+		Node node;
+
 		rwl.readLock().lock();
 		try {
-
 			if (id >= cache.size() || id < 0) {
 				logger.warn("Invalid id: " + id);
 				return null;
 			}
+			node = cache.get(id);
+		} finally {
+			rwl.readLock().unlock();
+		}
+		
+		synchronized (node) {
+			if (node.value == null) {
+				if (logger.isInfoEnabled()) {
+					logger.info("Read from disk node id: " + id);
+				}
 
-			Node node = cache.get(id);
-			synchronized (node) {
-
-				if (node.value == null) {
-					if (logger.isInfoEnabled()) {
-						logger.info("Read from disk node id: " + id);
+				rwl.writeLock().lock();
+				try {
+					while (node.size + memorySize > highMemoryLimit) {
+						noMemory.signalAll();
+						if (logger.isInfoEnabled()) {
+							logger.info("No room for node id: " + id + "; waiting");
+						}
+						memoryAvailable.awaitUninterruptibly();
 					}
 					node.value = loadFromFile(new Integer(id).toString());
-					memorySize.addAndGet(node.value.length);
-					memoryExpanded = true;
+					memorySize += node.size;
+				} finally {
+					rwl.writeLock().unlock();
 				}
-				node.usedCount++;
-				result = Arrays.copyOf(node.value, node.value.length);
 			}
-		} finally {
-			rwl.readLock().unlock();
+			node.usedCount++;
+			result = Arrays.copyOf(node.value, node.value.length);
 		}
 
-		if (memoryExpanded) {
-			ensureMemoryThreshold();
-		}
 		return result;
-	}
-
-	/**
-	 * if {@link #highMemoryLimit} exceeded then flushes least used node 
-	 *  to disk until memory fits {@link #lowMemoryLimit}
-	 */
-	private void ensureMemoryThreshold() {
-		if (memorySize.get() < highMemoryLimit) {
-			if (logger.isInfoEnabled()) {
-				logger.info("Memory size: " + memorySize.get() + " OK");
-			}
-			return;
-		}
-		logger.info("ensureMemoryThreshold");
-
-		rwl.readLock().lock();
-		try {
-			PriorityQueue<NodeSnapshot> queue = new PriorityQueue<>(cache.size());
-
-			logger.debug("Gather statistics");
-			for (int i = 0; i < cache.size(); i++) {
-				Node node = cache.get(i);
-				synchronized (node) {
-					if (node.value == null) {
-						continue;
-					}
-					queue.add(new NodeSnapshot(node, node.usedCount));
-				}
-			}
-
-			logger.debug("Clean up unused cache items");
-			for (NodeSnapshot nodeSnapshot = queue.poll(); nodeSnapshot != null; nodeSnapshot = queue.poll()) {
-				Node node = nodeSnapshot.node;
-				synchronized (node) {
-					if (node.value == null) {
-						continue;
-					}
-
-					if (!node.alreadyOnDisk) {
-						if (logger.isInfoEnabled()) {
-							logger.info("Flush to disk node id: " + node.id + ", size (bytes): " + node.value.length);
-						}
-						saveToFile(new Integer(node.id).toString(), node.value);
-					}
-					memorySize.addAndGet(-node.value.length);
-					node.value = null;
-				}
-				if (memorySize.get() <= lowMemoryLimit) {
-					break;
-				}
-			}
-		} finally {
-			rwl.readLock().unlock();
-		}
-	}
-	
-	private static class NodeSnapshot implements Comparable<NodeSnapshot> {
-		Node node;
-		int usedCount;
-
-		public NodeSnapshot(Node node, int size) {
-			super();
-			this.node = node;
-			this.usedCount = size;
-		}
-
-		@Override
-		public int compareTo(NodeSnapshot o) {
-			return Integer.compare(usedCount, o.usedCount);
-		}
 	}
 
 	public synchronized byte[] loadFromFile(String filename) {
@@ -212,5 +181,99 @@ public class MyCache {
 		disk.put(filename, data);
 
 	}
+	
+	private class Cleaner implements Runnable {
+
+		// BLOCKS-UNTIL: noMemory
+		@Override
+		public void run() {
+			while (true) {
+				rwl.writeLock().lock();
+				try {
+					noMemory.awaitUninterruptibly();
+				} finally {
+					rwl.writeLock().unlock();
+				}
+
+				freeMemory();
+			}
+		}
+
+		private void freeMemory() {
+			PriorityQueue<NodeSnapshot> queue;
+			rwl.readLock().lock();
+			try {
+				queue = new PriorityQueue<>(cache.size());
+
+				logger.debug("Gather statistics");
+				for (int i = 0; i < cache.size(); i++) {
+					Node node = cache.get(i);
+					synchronized (node) {
+						if (node.value == null) {
+							continue;
+						}
+						queue.add(new NodeSnapshot(node, i, node.usedCount));
+					}
+				}
+			} finally {
+				rwl.readLock().unlock();
+			}
+
+			logger.debug("Clean up unused cache items");
+			boolean cleanedEnough = false;
+			for (NodeSnapshot nodeSnapshot = queue.poll(); nodeSnapshot != null; nodeSnapshot = queue.poll()) {
+				Node node = nodeSnapshot.node;
+				synchronized (node) {
+					if (node.value == null) {
+						continue;
+					}
+
+					if (!node.alreadyOnDisk) {
+						if (logger.isInfoEnabled()) {
+							logger.info("Flush to disk node id: " + nodeSnapshot.id + ", size (bytes): " + node.size);
+						}
+						saveToFile(new Integer(nodeSnapshot.id).toString(), node.value);
+					}
+					node.value = null;
+				}
+
+				rwl.writeLock().lock();
+				try {
+					memorySize -= node.size;
+					if (memorySize <= lowMemoryLimit) {
+						cleanedEnough = true;
+					}
+					if (logger.isDebugEnabled()) {
+						logger.debug("Memory used: " + memorySize);
+					}
+					memoryAvailable.signalAll();
+					
+				} finally {
+					rwl.writeLock().unlock();
+				}
+				if (cleanedEnough) {
+					break;
+				}
+			}
+		}
+
+		private class NodeSnapshot implements Comparable<NodeSnapshot> {
+			Node node;
+			int usedCount;
+			int id;
+
+			public NodeSnapshot(Node node, int id, int usedCount) {
+				this.node = node;
+				this.id = id;
+				this.usedCount = usedCount;
+			}
+
+			@Override
+			public int compareTo(NodeSnapshot o) {
+				return Integer.compare(usedCount, o.usedCount);
+			}
+		}
+	}
+
 
 }
